3cd4c4f8f2e62c88a51ca9fe62a6663b20b869cc
Harpal Singh <harpal.rtu@gmail.com>
2019-07-25 15:33:34 +0530

    initial commit

---
+++ reactivepro/src/main/java/com/hpst/reactive/ReactiveproApplication.java
@@ -0,0 +1,13 @@
+package com.hpst.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveproApplication.class, args);
+	}
+
+}
